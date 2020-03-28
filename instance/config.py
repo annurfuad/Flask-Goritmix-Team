5872a5c6d07c288b8139f87f0f6e496989383209
annurfuad <annurfuad83@gmail.com>
2020-03-28 15:08:39 +0700

    added procfile

---
+++ instance/config.py
@@ -0,0 +1,3 @@
+SECRET_KEY = 'p9Bv<3Eid9%$i01ajsdkljfaskldfjaklf'
+#SQLALCHEMY_DATABASE_URI = 'mysql://dt_admin:dt2016@localhost/dreamteam_db' #For Mysql Datbase
+SQLALCHEMY_DATABASE_URI = 'postgresql://annurfuad:P4ssw0rd@localhost/sikewa_db' #For PostgreSQL Database
